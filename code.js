89e894ced250f7c05d4fa543e26bc9ffe930153e
func_vehicle <56104255+func-vehicle@users.noreply.github.com>
2020-07-25 13:37:45 +1200

    Reverted change to live code.js

--- code.js
+++ code.js
@@ -2208,7 +2208,6 @@ function playNotification(business, title, body) {
 window.notify = {
 	list: [],
 	id: 0,
-	registered: false,
 	log: function(msg) {
 		console.log(msg);
 	},
@@ -2233,39 +2232,32 @@ window.notify = {
 			});
 		}
 		else {
-			alert("Unfortunately, push notifications are not available for your browser.");
+			alert("Unfortunately, notifications are not available for your browser.");
 		}
 	},
 	show: function(title, body, business) {
-		if (notify.compatible()) {
-			// TODO:
-			if (!this.registered) {
-				navigator.serviceWorker.register('./sw.js');
-				registered = true;
-			}
-			navigator.serviceWorker.ready.then(function(registration) {
-				notify.id++;
-				var id = notify.id;
-				notify.list[id] = registration.showNotification(title, {
-					body: body,
-					tag: business,
-					icon: "img/"+business+".png",
-					lang: "",
-					dir: "auto",
-				});
-				notify.log("Notification #"+id+" queued for display");
-				notify.list[id].onclick = function() { notify.logEvent(id, "clicked"); };
-				notify.list[id].onshow  = function() { notify.logEvent(id, "showed");  };
-				notify.list[id].onerror = function() { notify.logEvent(id, "errored"); };
-				notify.list[id].onclose = function() { notify.logEvent(id, "closed");  };
-				
-				notify.log("Created a new notification...");
-				notify.log(notify.list[id]);
-				});
-			
-		}
-		else {
+		if (typeof Notification === "undefined") {
 			notify.log("Notifications not supported, ignoring.");
+			return;
+		}
+		if (notify.compatible()) {
+			notify.id++;
+			var id = notify.id;
+			notify.list[id] = new Notification(title, {
+				body: body,
+				tag: business,
+				icon: "img/"+business+".png",
+				lang: "",
+				dir: "auto",
+			});
+			notify.log("Notification #"+id+" queued for display");
+			notify.list[id].onclick = function() { notify.logEvent(id, "clicked"); };
+			notify.list[id].onshow  = function() { notify.logEvent(id, "showed");  };
+			notify.list[id].onerror = function() { notify.logEvent(id, "errored"); };
+			notify.list[id].onclose = function() { notify.logEvent(id, "closed");  };
+			
+			notify.log("Created a new notification...");
+			notify.log(notify.list[id]);
 		}
 	},
 	logEvent: function(id, event) {
