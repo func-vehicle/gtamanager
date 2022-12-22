a33de982c59564a7a82c065dc8b1dcb1ba876c59
Arsonide <arsonide@yahoo.com>
2022-12-22 10:33:40 -0600

    Fixed errant resource.

--- src/InfoContext.js
+++ src/InfoContext.js
@@ -401,7 +401,7 @@ export const staticInfo = {
         fullName: "Acid",
         shortName: "Acid",
         upgrades: ["equipment"],
-        resources: ["product", "supplies", "boost"],
+        resources: ["product", "supplies"],
         maxProduct: [360, 240],
         maxSupplies: [112.5, 150],
         maxBoost: [180, 120],
